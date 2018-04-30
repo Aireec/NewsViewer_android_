f2750c34ec04134af0ef4e420a0abea9288260ac
Aireec <aireecjoseph>
2018-04-30 19:35:53 +0200

    Added code for other buttons

--- app/src/main/java/com/example/user/newsviewer/MainActivity.java
+++ app/src/main/java/com/example/user/newsviewer/MainActivity.java
@@ -32,6 +32,69 @@ public class MainActivity extends AppCompatActivity {
                         startActivity(sportsnews);
 
                 }
+                Crimebtn = (Button) findViewById(R.id.Crimebtn);
+
+                Crimebtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        String crime = "https://www.news24.com/Tags/Topics/crime";
+                        Uri CrimeNews = Uri.parse(crime);
+
+                        Intent crimenews = new Intent(Intent.ACTION_VIEW, CrimeNews);
+                        if (crimenews.resolveActivity(getPackageManager()) != null) {
+                            startActivity(crimenews);
+                        }
+
+                        Politicsbtn = (Button) findViewById(R.id.Politicbtn);
+
+                        Politicsbtn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                                String politics = "https://www.news24.com/Tags/Topics/crime";
+                                Uri PoliticsNews = Uri.parse(politics);
+
+                                Intent politicsnews = new Intent(Intent.ACTION_VIEW, PoliticsNews);
+                                if (politicsnews.resolveActivity(getPackageManager()) != null ) {
+                                    startActivity(politicsnews);
+                                }
+                                Gamingbtn = (Button) findViewById(R.id.Gamebtn);
+
+                                Gamingbtn.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+
+                                        String game = "https://www.gamespot.com/news/";
+                                        Uri GamingNews = Uri.parse(game);
+
+                                        Intent gamingnews = new Intent(Intent.ACTION_VIEW, GamingNews);
+                                        if (gamingnews.resolveActivity(getPackageManager()) != null) {
+                                            startActivity(gamingnews);
+
+                                        }
+
+                                        Celebbtn = (Button) findViewById(R.id.Celebbtn);
+
+                                        Celebbtn.setOnClickListener(new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View v) {
+
+                                                String celeb = "https://www.channel24.co.za/Gossip";
+                                                Uri CelebNews = Uri.parse(celeb);
+
+                                                Intent celebnews = new Intent(Intent.ACTION_VIEW, CelebNews);
+                                                if (celebnews.resolveActivity(getPackageManager()) !=null) {
+                                                    startActivity(celebnews);
+                                                }
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
 
 
             }
